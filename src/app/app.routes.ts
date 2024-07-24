28747e48a03d9b37dc97cb73005bfee32cfaaec3
Roger Yuri de Araújo Lima <roger.lima@livsaude.com.br>
2024-07-24 15:33:53 -0300

    using routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AgendamentosComponent } from './agendamentos/agendamentos.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'home', component: HomeComponent },
+    { path: 'agendamentos', component: AgendamentosComponent },
+];
