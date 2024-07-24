a00a212b8263302fb2fe28a347e5eb46c5621d15
Roger Yuri de Araújo Lima <roger.lima@livsaude.com.br>
2024-07-24 09:23:51 -0300

    register screen and service are done

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -4,6 +4,9 @@ import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RegisterService } from './service/register.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { HttpClient } from '@angular/common/http';
+
 @Component({
   selector: 'app-register',
   standalone: true,
@@ -15,21 +18,44 @@ import { RegisterService } from './service/register.service';
 export class RegisterComponent {
   constructor(
     private registerService: RegisterService,
+    private _snackBar: MatSnackBar,
   ) { }
 
   register = new FormGroup({
     name: new FormControl(
       '',
-      Validators.required),
+      [
+        Validators.required,
+        Validators.nullValidator
+      ]),
     password: new FormControl(
       '',
-      Validators.required),
+      [
+        Validators.required,
+        Validators.nullValidator
+      ]),
     email: new FormControl(
       '',
-      Validators.required),
+      [
+        Validators.required,
+        Validators.email,
+        Validators.nullValidator,
+      ],
+    ),
   });
-  
+
   onSubmit() {
-    this.registerService.register(this.register.getRawValue());
+    this.registerService.register(this.register.getRawValue())
+      .subscribe({
+        next: () => {
+          this._snackBar.open('Register success', 'Close', { duration: 3000 })
+          this.register.reset()
+        },
+        error: e => {
+          const errorMessage = e.error?.message || 'Erro desconhecido';
+          this._snackBar.open('Register failed: ' + JSON.stringify(errorMessage), 'Close', { duration: 3000 });
+          console.log(JSON.stringify(errorMessage));
+        }
+      });;
   }
 }
--- src/app/register/service/register.service.ts
+++ src/app/register/service/register.service.ts
@@ -1,24 +1,13 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Register } from "../register";
-import { catchError } from "rxjs";
-import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Injectable({ providedIn: 'root' })
 export class RegisterService {
     constructor(
         private http: HttpClient, 
-        private _snackBar: MatSnackBar,
     ) { }
     register(data: Register) {
-        this.http.post('http://localhost:8080/auth/register', data).pipe(
-            catchError((error) => {
-                console.log('Error:', error);
-                throw error;
-            })
-        ).subscribe(register => {
-            this._snackBar.open('Register success', 'Close', { duration: 3000 });
-        },
-        );
+        return this.http.post('http://localhost:8080/auth/register', data)
     }
 }
